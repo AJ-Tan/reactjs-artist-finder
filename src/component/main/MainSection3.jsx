95ec91f50137c7c364539921bc49f26f9c4be3d2
AJ-Tan <aldomin22@gmail.com>
2023-09-14 06:05:26 +0800

    updated media and maincomponent3 structure

--- src/component/main/MainSection3.jsx
+++ src/component/main/MainSection3.jsx
@@ -1,36 +1,33 @@
+import { useState } from 'react';
+import ItemContainer from './mainsection3_components/ItemContainer';
+
 const MainSection3 = () => {
+  const [itemList, setItemList] = useState([
+    {
+      fontawesomeIcon: 'fa-solid fa-magnifying-glass',
+      heading: 'Find your artist',
+      description: 'Use our search engine to find any artist easily',
+    },
+    {
+      fontawesomeIcon: 'fa-solid fa-calendar-days',
+      heading: 'Book and pay',
+      description: 'Schedule your artist and pay safely here',
+    },
+    {
+      fontawesomeIcon: 'fa-solid fa-champagne-glasses',
+      heading: 'Enjoy the show!',
+      description: "Sit back and relax knowing it's all done",
+    },
+  ]);
+
   return (
     <section className="main-section-3">
       <h3 className="main-section-3-heading">How It Works</h3>
 
       <div className="items-container">
-        <div className="item-container">
-          <div className="icon-container icon-c-1">
-            <i className="fa-solid fa-magnifying-glass m-s3-icon m-s3-icon1"></i>
-          </div>
-          <h4 className="item-heading">Find your artist</h4>
-          <p className="item-description">
-            Use our search engine to find any artist easily
-          </p>
-        </div>
-        <div className="item-container">
-          <div className="icon-container icon-c-2">
-            <i className="fa-solid fa-calendar-days m-s3-icon m-s3-icon2"></i>
-          </div>
-          <h4 className="item-heading">Book and pay</h4>
-          <p className="item-description">
-            Schedule your artist and pay safely here
-          </p>
-        </div>
-        <div className="item-container">
-          <div className="icon-container icon-c-3">
-            <i className="fa-solid fa-champagne-glasses m-s3-icon m-s3-icon3"></i>
-          </div>
-          <h4 className="item-heading">Enjoy the show!</h4>
-          <p className="item-description">
-            Sit back and relax knowing it's all done
-          </p>
-        </div>
+        {itemList.map((item) => (
+          <ItemContainer {...item} />
+        ))}
       </div>
 
       <p className="more-info">
